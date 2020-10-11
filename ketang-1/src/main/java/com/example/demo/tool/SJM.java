27a8f9f827bcea49743d4acd5cefe88c47a19c80
manmen1221 <manmen1221@163.com>
2020-10-11 21:46:38 +0800

    修正了复随机码数值少1的小bug(数字没有9 字母没有z)

--- ketang-1/src/main/java/com/example/demo/tool/SJM.java
+++ ketang-1/src/main/java/com/example/demo/tool/SJM.java
@@ -1,21 +1,22 @@
 package com.example.demo.tool;
 
+//随机码
 public class SJM {
 	
 	//取数字随机码(位数) 用于签到码
 	public static String getNum(int size) {
 		String temp="";
 		for(int i=0;i<size;i++) {
-			temp=temp+((int)(Math.random()*9));
+			temp=temp+((int)(Math.floor(Math.random()*10)));
 		}
 		return temp;
 	}
 	
-	//取数字加字母大写() 用于课程码
+	//取数字加字母大写(位数) 用于课程码
 	public static String getNumLet(int size) {
 		String temp="";
 		for(int i=0;i<size;i++) {
-			int temp2=((int)(Math.random()*35));
+			int temp2=((int)(Math.floor(Math.random()*36)));
 			if(temp2>9)temp+=(char)(temp2-10+'A');
 			else temp+=temp2;
 		}
--- ketang-1/src/main/java/com/example/demo/tool/Time.java
+++ ketang-1/src/main/java/com/example/demo/tool/Time.java
@@ -3,15 +3,24 @@ package com.example.demo.tool;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+//系统时间转字符串
 public class Time {
+	
+	//日期格式
 	private static SimpleDateFormat s= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+	//取当前时间
 	public static String getTime() {
 		return s.format(new Date());
 	}
+	
+	//取(毫秒)之后的时间
 	public static String getTimeNext(long time) {
 		Date temp=new Date();
 		return s.format(new Date(temp.getTime() + time));
 	}
+	
+	//取(毫秒)之前后的时间
 	public static String getTimeBefor(long time) {
 		Date temp=new Date();
 		return s.format(new Date(temp.getTime() - time));
